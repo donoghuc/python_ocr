45db335eb58695962b538411cb533659c87636c4
donoghuc <cas.donoghue@gmail.com>
2018-01-21 10:10:55 -0800

    make OCR for vm malware work public

---
+++ snip_to_text.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+
+
+def parse_args():
+    ''' define CLI params'''
+    parser = argparse.ArgumentParser(description='Optical Character Recogonition')
+    parser.add_argument('-i','--image', help='imput image file (png)', required=True)
+    parser.add_argument('-o','--output', help='output text file')
+    return vars(parser.parse_args())
+
+
+def get_text(image_file):
+    ''' do minimal pre-processing and then get the text'''
+   
+    image = cv2.imread(image_file)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    temp_file = "temp_file.png"
+    cv2.imwrite(temp_file, gray)
+    text = pytesseract.image_to_string(Image.open(temp_file))
+    os.remove(temp_file)
+
+    return text
+
+
+def main():
+    args = parse_args()
+    text = get_text(args['image'])
+    print(text)
+
+    if args['output']:
+        with open(args['output'], "w") as fo:
+            fo.write(text)
+
+
+if __name__ == '__main__':
+    main()
